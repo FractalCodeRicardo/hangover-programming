67360673972cd5c8ad0def94481ac5fe06094e83
Ricardo Serrato B. <rsbse@hotmail.com>
2025-06-29 16:06:50 -0600

    changes

--- obstacles/src/main.rs
+++ obstacles/src/main.rs
@@ -1,9 +1,10 @@
-use std::fs::{self};
+use std::fs;
 
 use macroquad::prelude::*;
+
 const SQUARE_SIZE: f32 = 10.0;
-const DELAY: f64 = 0.3;
-const SPEED: f32 = 1.0;
+const DELAY: f64 = 0.1;
+
 struct Position {
     x: f32,
     y: f32,
@@ -13,51 +14,98 @@ impl Position {
     fn new(x: f32, y: f32) -> Position {
         Position { x: x, y: y }
     }
+
+    fn is_same(&self, pos: &Position) -> bool{
+        return self.x == pos.x && self.y == pos.y;
+    }
 }
 
 struct Game {
-    obstacles: Vec<Position>,
     ball: Position,
-    ball_direction: Position,
+    ball_dir: Position,
+    obstacles: Vec<Position>,
 }
 
 impl Game {
     fn new() -> Game {
         Game {
-            obstacles: Game::init_obstacles(),
-            ball: Position::new(5.0, 1.0),
-            ball_direction: Position::new(1.0, 1.0),
+            ball: Position::new(5.0, 5.0),
+            ball_dir: Position::new(0.0, 1.0),
+            obstacles: Game::create_obstacles(),
         }
     }
 
     fn mov(&mut self) {
         self.handle_crash();
-        self.ball.x += self.ball_direction.x * SPEED;
-        self.ball.y += self.ball_direction.y * SPEED;
+        self.ball.x += self.ball_dir.y;
+        self.ball.y += self.ball_dir.x;
     }
 
-    fn is_crash_up(&self) -> bool {
+    fn draw(&self) {
+        self.draw_ball();
+        self.draw_obstacles();
+    }
+
+    fn draw_ball(&self) {
+        Game::draw_square(&self.ball);
+    }
+
+    fn draw_obstacles(&self) {
         for obs in &self.obstacles {
-            let dx = obs.x + SQUARE_SIZE / 2.0;
-            let dy = obs.y * SQUARE_SIZE;
+            Game::draw_square(obs);
+        }
+    }
 
-            let bx = self.ball.x + SQUARE_SIZE / 2.0;
-            let by = self.ball.y * SQUARE_SIZE;
+    fn draw_square(position: &Position) {
+        draw_rectangle(
+            position.x * SQUARE_SIZE,
+            position.y * SQUARE_SIZE,
+            SQUARE_SIZE,
+            SQUARE_SIZE,
+            MAGENTA,
+        );
+    }
 
-            if dx == bx && dy == by {
-                return true;
+    fn create_obstacles() -> Vec<Position> {
+        let file = fs::read_to_string("./src/obstacles.txt").unwrap_or_else(|err| {
+            eprintln!("Failed to read file: {}", err);
+            std::process::exit(1);
+        });
+
+        let list: Vec<_> = file.lines().map(|line| line.to_string()).collect();
+        let mut positions: Vec<Position> = Vec::new();
+        let mut y: f32 = 0.0;
+        let mut x: f32 = 0.0;
+
+        for line in list {
+            x = 0.0;
+            for c in line.chars() {
+                if c == '.' {
+                    positions.push(Position { x: x, y: y });
+                }
+                x += 1.0;
             }
+            y += 1.0;
         }
 
-        return false;
+        return positions;
     }
 
     fn top_center(position: &Position)-> Position {
-        let x = position.x * SQUARE_SIZE;
-        let y = position.y * SQUARE_SIZE + SQUARE_SIZE / 2.0;
+        let x = position.x * SQUARE_SIZE + SQUARE_SIZE / 2.0;
+        let y = position.y * SQUARE_SIZE;
 
         return Position {x: x, y: y};
     }
+
+    fn bottom_center(position: &Position)-> Position {
+        let x = position.x * SQUARE_SIZE;
+        let y = position.y * SQUARE_SIZE+ SQUARE_SIZE / 2.0;
+
+        return Position {x: x, y: y};
+    }
+
+
     fn left_center(position: &Position)-> Position {
 
         let x = position.x * SQUARE_SIZE;
@@ -66,7 +114,6 @@ impl Game {
         return Position {x: x, y: y};
     }
 
-
     fn right_center(position: &Position)-> Position {
         let x = position.x * SQUARE_SIZE + SQUARE_SIZE;
         let y = position.y * SQUARE_SIZE + SQUARE_SIZE / 2.0;
@@ -74,45 +121,33 @@ impl Game {
         return Position {x: x, y: y};
     }
 
-    fn is_crash_down(&self) -> bool {
+    fn is_crash_horizontal(&self) -> bool {
         for obs in &self.obstacles {
-            let bx = obs.x + SQUARE_SIZE / 2.0;
-            let by = obs.y * SQUARE_SIZE;
+            
+            let ro = Game::right_center(obs);
+            let lo = Game::left_center(obs);
 
-            let dx = self.ball.x + SQUARE_SIZE / 2.0;
-            let dy = self.ball.y * SQUARE_SIZE;
+            let rb = Game::right_center(&self.ball);
+            let lb = Game::left_center(&self.ball);
 
-            if dx == bx && dy == by {
+            if rb.is_same(&lo) || lb.is_same(&ro) {
                 return true;
             }
         }
         return false;
     }
 
-    fn is_crash_right(&self) -> bool {
+
+    fn is_crash_vertical(&self) -> bool {
         for obs in &self.obstacles {
-            let lox = obs.x * SQUARE_SIZE;
-            let loy = obs.y + SQUARE_SIZE / 2.0;
+            
+            let to = Game::top_center(obs);
+            let bo = Game::bottom_center(obs);
 
-            let rbx = self.ball.x * SQUARE_SIZE + SQUARE_SIZE;
-            let rby = self.ball.y + SQUARE_SIZE / 2.0;
+            let tb = Game::top_center(&self.ball);
+            let bb = Game::bottom_center(&self.ball);
 
-            if lox == rbx && loy == rby {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn is_crash_left(&self) -> bool {
-        for obs in &self.obstacles {
-            let rbx = obs.x * SQUARE_SIZE;
-            let rby = obs.y + SQUARE_SIZE / 2.0;
-
-            let lox = self.ball.x * SQUARE_SIZE + SQUARE_SIZE;
-            let loy = self.ball.y + SQUARE_SIZE / 2.0;
-
-            if lox == rbx && loy == rby {
+            if bo.is_same(&tb) || to.is_same(&bb) {
                 return true;
             }
         }
@@ -120,113 +155,40 @@ impl Game {
     }
 
     fn handle_crash(&mut self) {
-        if self.is_crash_up() {
-            self.changey_direction();
-            return;
+
+        if self.is_crash_vertical() {
+            self.change_vertical_direction();
+            return
         }
 
-        if self.is_crash_down() {
-            self.changey_direction();
-            return;
-        }
 
-        if self.is_crash_right() {
-            self.changex_direction();
-            return;
-        }
-
-        if self.is_crash_left() {
-            self.changex_direction();
-            return;
+        if self.is_crash_horizontal() {
+            self.change_horizontal_direction();
+            return
         }
     }
-
-    fn changey_direction(&mut self) {
-        self.ball_direction.y = self.ball_direction.y * -1.0;
+    
+    fn change_vertical_direction(&mut self) {
+        self.ball_dir.y = self.ball_dir.y * -1.0;
     }
 
-    fn changex_direction(&mut self) {
-        self.ball_direction.x = self.ball_direction.x * -1.0;
-    }
-
-    fn get_content_file() -> Vec<String> {
-        let file = fs::read_to_string("./src/board.txt").unwrap_or_else(|err| {
-            eprintln!("Failed to read file: {}", err);
-            std::process::exit(1);
-        });
-
-        let list: Vec<_> = file.lines().map(|line| line.to_string()).collect();
-
-        return list;
-    }
-
-    fn init_obstacles() -> Vec<Position> {
-        let lines = Game::get_content_file();
-        let mut positions: Vec<Position> = Vec::new();
-        let mut y: usize = 0;
-
-        for l in lines {
-            let mut x: usize = 0;
-            for c in l.chars() {
-                if c == '.' {
-                    positions.push(Position::new(x as f32, y as f32));
-                }
-                x += 1;
-            }
-            y += 1;
-        }
-
-        return positions;
-    }
-
-    fn draw(&self) {
-        self.draw_positions();
-        self.draw_ball();
-    }
-
-    fn draw_positions(&self) {
-        for obs in &self.obstacles {
-            Game::draw_square(obs.x as f32, obs.y as f32);
-        }
-    }
-
-    fn draw_square(x: f32, y: f32) {
-        draw_rectangle(
-            x * SQUARE_SIZE,
-            y * SQUARE_SIZE,
-            SQUARE_SIZE,
-            SQUARE_SIZE,
-            WHITE,
-        );
-    }
-
-    fn draw_ball(&self) {
-        Game::draw_square(self.ball.x as f32, self.ball.y as f32);
-    }
-
-    fn print(&self) {
-        println!("Ball ({}, {})", self.ball.x, self.ball.y);
-        println!(
-            " Direction ({}, {})",
-            self.ball_direction.x, self.ball_direction.y
-        );
+    fn change_horizontal_direction(&mut self) {
+        self.ball_dir.x = self.ball_dir.x * -1.0;
     }
 }
-
 #[macroquad::main("MyGame")]
 async fn main() {
     let mut game = Game::new();
-    let time = get_time();
-    loop {
-        clear_background(BLACK);
+    let mut time = get_time();
 
+    loop {
+        clear_background(DARKBLUE);
         game.draw();
 
         if get_time() - time > DELAY {
-            game.print();
             game.mov();
+            time = get_time();
         }
-
-        next_frame().await
+        next_frame().await;
     }
 }
