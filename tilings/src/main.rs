6bee8798918555ae8cd944935f8966ab1d945065
Ricardo Serrato B. <rsbse@hotmail.com>
2025-07-12 10:58:58 -0600

    tilings

--- tilings/src/main.rs
+++ tilings/src/main.rs
@@ -1,35 +1,45 @@
 use macroquad::prelude::*;
 
 const SIZE: f32 = 10.0;
-const SIDES: f32 = 5.0;
+const SIDES: u8 = 8;
+const ROTATION: f32 = 5.0;
+
 
 #[macroquad::main("MyGame")]
 async fn main() {
-    let width = screen_width();
-    let height = screen_height();
+    let w = screen_width();
+    let h = screen_height();
 
     let mut x: f32 = 0.0;
     let mut y: f32 = 0.0;
+    let mut r: f32 = 0.0;
+    let mut s: u8 = 3;
+    let mut tilings: Vec<(f32, f32, f32, u8)> = Vec::new();
 
-    let mut positions: Vec<(f32, f32)> = Vec::new();
-    while y < width {
+    while y < h*1.5 {
         x = 0.0;
-        while x < height {
-            positions.push((x, y));
-            x += SIZE;
-        }
 
+        while x < w {
+            x += SIZE;
+            tilings.push((x, y, r, s));
+            r += ROTATION;
+            s += 1;
+
+            if s > SIDES {
+                s = 3;
+            }
+
+        }
         y += SIZE;
     }
 
     loop {
-        clear_background(BLACK);
+        clear_background(RED);
 
-        for p in &positions {
-            draw_poly(p.0, p.1, SIZE as uj/2.0 as u8, 30.0, 0.0, WHITE);
+        for t in &tilings{
+            draw_poly(t.0, t.1, t.3, SIZE / 2.0, t.2, GREEN);
         }
 
-        next_frame().await;
+        next_frame().await
     }
 }
-
