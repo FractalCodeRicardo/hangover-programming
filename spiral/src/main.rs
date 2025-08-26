f80d5d97559cc4c5a07de58ce33951d5a6e7a596
Ricardo Serrato B. <rsbse@hotmail.com>
2025-08-25 20:47:17 -0600

    Spiral

--- spiral/src/main.rs
+++ spiral/src/main.rs
@@ -1,37 +1,44 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("My Macroquad Game")]
+#[macroquad::main("My Game")]
 async fn main() {
     let mut r: f32 = 0.0;
     let mut angle: f32 = 0.0;
     let alpha: f32 = 1.0;
-
-    let mut x = 0.0;
-    let mut y = 0.0;
+    let mut x: f32 = 0.0;
+    let mut y: f32 = 0.0;
+    let every_point = 10;
 
     let mut points: Vec<(f32, f32)> = Vec::new();
-    loop {
-        clear_background(BLACK); // Clear the screen with a red background
-        r = alpha * angle;
 
-        x = (screen_width() / 2.0) + r * angle.sin();
-        y = (screen_height() / 2.0) + r * angle.cos();
+    loop {
+        r = alpha * angle;
+        angle += 0.5;
+
+        y = r * angle.sin();
+        x = r * angle.cos();
+
+        y = y + screen_height() / 2.0;
+        x = x + screen_width() / 2.0;
 
         points.push((x, y));
 
-        angle = angle + 0.5;
+        clear_background(BLACK);
 
+        let mut i = 0;
         for p in &points {
-            draw_rectangle(
-                 p.0,
-                 p.1,
-                2.0,
-                2.0,
-                WHITE,
-            ); 
+            draw_rectangle(p.0, p.1, 2.0, 2.0, WHITE);
 
-            draw_circle(p.0, p.1, 10.0, WHITE);
+            if i == every_point {
+
+                draw_poly_lines(p.0, p.1, 6, r / 3.0, angle, 1.0, GREEN);
+
+                i = 0;
+            }
+
+            i += 1;
         }
-        next_frame().await; // Wait for the next frame
+
+        next_frame().await
     }
 }
