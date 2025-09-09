c02a34a40aa3a614ab4f9f77531497689c2a5630
Ricardo Serrato B. <rsbse@hotmail.com>
2025-09-08 21:27:58 -0600

    invaders

--- space_invader/src/main.rs
+++ space_invader/src/main.rs
@@ -1,7 +1,8 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*, rand::RandomRange};
 
 const SQUARE_SIZE: f32 = 20.0;
-const DELAY: f64 = 1.0;
+const DELAY: f64 = 0.2;
+const INVADER_EVERY:u16 = 2;
 
 struct Position {
     x: f32,
@@ -22,6 +23,35 @@ impl Position {
     }
 }
 
+struct Invader {
+    position: Position,
+    speed: f32
+}
+
+impl  Invader {
+
+    pub fn new() -> Self {
+        Invader{
+            position: Position::new(
+                RandomRange::gen_range(0.0, screen_width()),
+                0.0
+            ),
+            speed: 6.0
+        }
+    }
+
+    pub fn draw(&self) {
+        let x = self.position.x;
+        let y = self.position.y;
+        draw_rectangle(x, y, SQUARE_SIZE , SQUARE_SIZE, RED);
+    }
+
+    pub fn mov(& mut self) {
+        self.position.add(0.0, self.speed);
+    }
+    
+}
+
 struct Bullet {
     position: Position,
     speed: f32
@@ -34,12 +64,12 @@ impl  Bullet {
         let pos = Position::new(x, y);
         Bullet {
             position : pos,
-            speed: 1.0
+            speed: 6.0
         }
     }
 
     pub fn mov(& mut self) {
-        self.position.add(0.0, SQUARE_SIZE * -1.0);
+        self.position.add(0.0, -1.0 * self.speed);
     }
 
     pub fn draw(&self) {
@@ -90,7 +120,9 @@ impl Ship {
 
 struct Game {
     ship: Ship,
-    bullets: Vec<Bullet>
+    bullets: Vec<Bullet>,
+    invaders: Vec<Invader>,
+    invader_count: u16
 }
 
 impl Game {
@@ -98,17 +130,22 @@ impl Game {
     pub fn new() -> Self {
         return Game {
             ship: Ship::new(),
-            bullets: Vec::new()
+            bullets: Vec::new(),
+            invaders: Vec::new(),
+            invader_count: 0
         }
     }
 
     pub fn draw(& self) {
         self.ship.draw();
         self.draw_bullets();
+        self.draw_invaders();
     }
 
     pub fn mov(&mut self) {
         self.mov_bullets();
+        self.mov_invaders();
+        self.create_invader();
     }
 
     pub fn mov_bullets(& mut self) {
@@ -117,9 +154,31 @@ impl Game {
         }
     }
 
+    pub fn mov_invaders(& mut self) {
+        for invader in & mut self.invaders {
+            invader.mov();
+        }
+    }
+
+    pub fn create_invader(& mut self) {
+        if self.invader_count >= INVADER_EVERY {
+            self.invaders.push(Invader::new());
+            self.invader_count = 0;
+            return;
+        }
+
+        self.invader_count += 1;
+    }
+
     pub fn draw_bullets(& self) {
         for bullet in &self.bullets {
             bullet.draw();
+        }
+    }
+
+    pub fn draw_invaders(& self) {
+        for invader in &self.invaders {
+            invader.draw();
         }
     }
 
