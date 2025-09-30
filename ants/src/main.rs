cc5eb46020712bcaf577d8855bd47f51270ce7fb
Ricardo Serrato B <rsbse@hotmail.com>
2025-09-30 16:10:53 -0600

    Ants

--- ants/src/main.rs
+++ ants/src/main.rs
@@ -1,8 +1,8 @@
 use macroquad::{prelude::*, rand::RandomRange};
 
 const SIZE: f32 = 100.0;
-const SQUARE_SIZE: f32 = 5.0;
-const ANTS_NUMBER: usize = 50;
+const SQUARE_SIZE: f32 = 10.0;
+const ANTS_NUMBER: usize = 200;
 
 struct Position {
     x: f32,
@@ -33,118 +33,126 @@ struct Ant {
 
 impl Ant {
     fn new() -> Self {
-        // let pos = Position::new(
-        //     1.0, 
-        //     1.0
-        // );
-        //
-        // let dir = Direction::new(
-        //     1.0, 
-        //     0.0
-        // );
-
-        let pos = Position::new(
-            RandomRange::gen_range(0.0, SIZE-1.0),
-            RandomRange::gen_range(0.0, SIZE-1.0),
-        );
-
-        let dir = Direction::new(
-            Ant::random_binary(), 
-            Ant::random_binary()
-        );
-
+        // let pos = Position::new(1.0, 1.0);
+        // let dir = Direction::new(0.0, 1.0);
+        
+        let pos = Ant::random_position();
+        let dir = Ant::random_direction();
 
         return Ant { pos: pos, dir: dir };
     }
 
-    fn random_binary() -> f32 {
-        let number = RandomRange::gen_range(0.0, 1.0);
-        if number > 0.5 { 1.0 } else { 0.0 }
+    fn random_position() -> Position {
+        let x = RandomRange::gen_range(0.0, SIZE -1.0);
+        let y = RandomRange::gen_range(0.0, SIZE -1.0);
+
+        Position {
+            x:x,
+            y:y
+        }
+    }
+
+    fn random_direction() -> Direction{
+        let x = if RandomRange::gen_range(0.0, 1.0) >= 0.5 {1.0} else {0.0};
+        let y = if RandomRange::gen_range(0.0, 1.0) >= 0.5 {1.0} else {0.0};
+
+        Direction {
+            x:x,
+            y:y
+        }
+    }
+
+    fn draw(&self) {
+        draw_circle(
+            self.pos.x * SQUARE_SIZE + SQUARE_SIZE / 2.0,
+            self.pos.y * SQUARE_SIZE + SQUARE_SIZE / 2.0,
+            SQUARE_SIZE / 2.0,
+            RED,
+        );
     }
 
     fn mov(&mut self) {
         let mut nx = self.pos.x + self.dir.x;
         let mut ny = self.pos.y + self.dir.y;
 
-        nx = nx.max(0.0);
-        ny = ny.max(0.0);
+        if nx > SIZE - 1.0 {
+            nx = 0.0;
+        }
 
-        nx = nx.min(SIZE - 1  as f32);
-        ny = ny.min(SIZE - 1 as f32);
+        if nx < 0.0 {
+            nx = SIZE - 1.0;
+        }
 
-        self.pos = Position::new(nx, ny)
+        if ny > SIZE - 1.0 {
+            ny = 0.0;
+        }
+
+        if ny < 0.0 {
+            ny = SIZE - 1.0;
+        }
+
+        self.pos = Position::new(nx, ny);
     }
 
-    fn move_left(&mut self) {
-        let left = self.left_direction();
-        self.dir = left;
+    fn turn_right(&mut self) {
+        self.dir = self.right_direction();
         self.mov();
     }
 
-    fn move_right(&mut self) {
-        let right = self.right_direction();
-        self.dir = right;
+    fn turn_left(&mut self) {
+        self.dir = self.left_direction();
         self.mov();
     }
 
-    fn right_direction(&self) -> Direction {
+    fn right_direction(&mut self) -> Direction {
+
         if self.dir.y == -1.0 {
             return Direction::new(1.0, 0.0);
-        }
+        } 
 
         if self.dir.x == 1.0 {
-            return Direction::new(0.0, 1.0);
-        }
+            return Direction::new(0.0, 1.0)
+        } 
 
         if self.dir.y == 1.0 {
             return Direction::new(-1.0, 0.0);
-        }
+        } 
 
         return Direction::new(0.0, -1.0);
     }
 
-    fn left_direction(&self) -> Direction {
+
+    fn left_direction(&mut self) -> Direction {
+
         if self.dir.y == -1.0 {
             return Direction::new(-1.0, 0.0);
-        }
+        } 
 
-        if self.dir.x == 1.0 {
-            return Direction::new(0.0, -1.0);
-        }
+        if self.dir.x == -1.0 {
+            return Direction::new(0.0, 1.0)
+        } 
 
         if self.dir.y == 1.0 {
             return Direction::new(1.0, 0.0);
-        }
+        } 
 
-        return Direction::new(0.0, 1.0);
-    }
-
-    fn draw(&self) {
-        draw_circle(
-            (self.pos.x * SQUARE_SIZE) + SQUARE_SIZE / 2.0,
-            (self.pos.y * SQUARE_SIZE) + SQUARE_SIZE / 2.0,
-            SQUARE_SIZE / 2.0,
-            RED,
-        )
+        return Direction::new(0.0, -1.0);
     }
 }
 
 struct Square {
     pos: Position,
-    color: i32,
+    color: usize
 }
 
 impl Square {
     fn new(x: f32, y: f32) -> Self {
-        return Square {
-            pos: Position::new(x, y),
-            color: 0,
-        };
+        let pos = Position::new(x, y);
+        return Square { pos: pos, color: 1 };
     }
 
     fn draw(&self) {
-        let color = if self.color == 1 { WHITE } else { BLACK};
-
+        let color = if self.color == 1 {WHITE} else {BLACK};
         draw_rectangle(
             self.pos.x * SQUARE_SIZE,
             self.pos.y * SQUARE_SIZE,
@@ -155,47 +163,68 @@ impl Square {
     }
 
     fn change_color(&mut self) {
-        let new_color = if self.color == 1 { 0 } else { 1 };
-
+        let new_color = if self.color ==1 {0} else {1};
         self.color = new_color;
+
     }
 }
 
 struct Game {
-    ants: Vec<Ant>,
     squares: Vec<Square>,
+    ants: Vec<Ant>,
 }
 
 impl Game {
     fn new() -> Self {
-        return Game {
-            ants: Game::random_ants(),
+        Game {
             squares: Game::create_squares(),
-        };
-    }
-
-    fn random_ants() -> Vec<Ant> {
-        let mut ants: Vec<Ant> = Vec::new();
-
-        for i in 0..ANTS_NUMBER {
-            ants.push(Game::random_ant());
+            ants: Game::create_ants(),
         }
-
-        return ants;
-    }
-
-    fn random_ant() -> Ant {
-        return Ant::new();
     }
 
     fn create_squares() -> Vec<Square> {
         let mut squares: Vec<Square> = Vec::new();
         for j in 0..SIZE as usize {
             for i in 0..SIZE as usize {
-                squares.push(Square::new(i as f32, j as f32))
+                let s = Square::new(i as f32, j as f32);
+                squares.push(s);
             }
         }
+
         return squares;
+    }
+
+    fn create_ants() -> Vec<Ant> {
+        let mut ants: Vec<Ant> = Vec::new();
+
+        for i in 0..ANTS_NUMBER {
+            ants.push(Ant::new());
+        }
+
+        return ants;
+    }
+
+    fn mov_ants(&mut self) {
+        for a in &mut self.ants {
+
+            let i = a.pos.x as usize;
+            let j = a.pos.y as usize;
+            let size = SIZE as usize;
+
+            let s = &mut self.squares[j*size +i];
+
+            if s.color == 1 {
+                a.turn_right();
+            } else {
+                a.turn_left();
+            }
+
+            s.change_color();
+        }
+    }
+
+    fn mov(&mut self) {
+        self.mov_ants();
     }
 
     fn draw(&self) {
@@ -204,35 +233,14 @@ impl Game {
     }
 
     fn draw_squares(&self) {
-        for square in &self.squares {
-            square.draw();
+        for s in &self.squares {
+            s.draw();
         }
     }
 
     fn draw_ants(&self) {
-        for ant in &self.ants {
-            ant.draw();
-        }
-    }
-
-    fn mov(&mut self) {
-        self.mov_ants();
-    }
-
-    fn mov_ants(&mut self) {
-        for ant in &mut self.ants {
-            let i = ant.pos.x as usize;
-            let j = ant.pos.y as usize;
-            let index = j * SIZE as usize + i;
-            let square = &mut self.squares[index];
-
-            if square.color == 1 {
-                ant.move_right();
-            } else {
-                ant.move_left();
-            }
-
-            square.change_color();
+        for a in &self.ants {
+            a.draw();
         }
     }
 }
