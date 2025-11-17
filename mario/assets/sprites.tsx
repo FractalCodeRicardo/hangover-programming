27b66b8e7da5e49d094fe744753bb7330ef619a3
Ricardo Serrato B. <rsbse@hotmail.com>
2025-11-17 10:53:51 -0600

    Assets updated

---
+++ mario/assets/sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="running.png" tilewidth="32" tileheight="32" tilecount="234" columns="18">
+ <image source="sprites.png" width="584" height="436"/>
+</tileset>
