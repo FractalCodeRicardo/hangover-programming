f6c0998a66424ca79e4a00ce7dc4fdba25e5ce0c
Ricardo Serrato B. <rsbse@hotmail.com>
2026-05-20 09:44:42 -0600

    Camera errors fixedx

--- 3d/world.c
+++ 3d/world.c
@@ -3,24 +3,48 @@
 #include "raymath.h"
 #include <stdio.h>
 
+const float TARGET_DISTANCE = 4.0f;
+const float SPEED = 1.0f;
+
 typedef struct {
   Camera3D camera;
+  Vector3 direction;
 } Game;
 
-Camera3D GetCamera() {
+void RefreshTarget(Camera3D *camera, Vector3 direction) {
+  Vector3 target = Vector3Scale(direction, TARGET_DISTANCE);
+  target = Vector3Add(camera ->position, target);
+  camera -> target = target; 
+}
+
+Camera3D GetCamera(Vector3 direction) {
+  Vector3 position =  (Vector3){0, 4, 0};
+
   Camera3D camera = {0};
-  camera.position = (Vector3){0.0f, 4.0f, 4.0f};
-  camera.target = (Vector3){0.0f, 4, 3};
+  camera.position = position;
   camera.up = (Vector3){0.0f, 1.0f, 0.0f};
   camera.fovy = 60.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
+  RefreshTarget(&camera, direction);
+
   return camera;
+}
+
+Vector3 InitialDirection() {
+  Vector3 direction = (Vector3) {
+    1, 0, 1
+  };
+
+  direction = Vector3Normalize(direction);
+  direction = Vector3Scale(direction,  SPEED);
+  return direction;
 }
 
 Game GetGame() {
   Game game;
-  game.camera = GetCamera();
+  game.direction = InitialDirection();
+  game.camera = GetCamera(game.direction);
 
   return game;
 }
@@ -29,78 +53,49 @@ void DrawGame(Game *game) {
   DrawGrid(100, 1.0f); 
 }
 
-void MoveVector(Game *game, Vector3 v) {
-  Vector3 pos = game->camera.position;
-  Vector3 target = game->camera.target;
+void MoveVector(Game *game, Vector3 direction) {
+  Vector3 position = Vector3Scale(direction, SPEED);
+  position = Vector3Add(game->camera.position, position);
+  game -> camera.position = position;
 
-  game -> camera.position = (Vector3) {
-    pos.x + v.x,
-    pos.y + v.y,
-    pos.z + v.z
-  };
-
-
-  game -> camera.target = (Vector3) {
-    target.x + v.x,
-    target.y + v.y,
-    target.z + v.z
-  };
+  RefreshTarget(&game ->camera, game->direction);
 }
 
-Vector3 forwardDirection(Game *game) {
-  Vector3 target = game -> camera.target;
-  Vector3 position = game -> camera.position;
-
-  Vector3 direction = Vector3Subtract(target, position);
-
-  return direction;
-}
 
 void flip(Game *game, float size) {
   float sensivity = 0.005f;
   float rads = size * sensivity;
 
   Vector3 position = game -> camera.position;
-  Vector3 forward = forwardDirection(game);
-
   Vector3 rotated = Vector3RotateByAxisAngle(
-      forward, 
+      game -> direction, 
       (Vector3){0, 1, 0},
       rads
     );
 
-  game -> camera.target = Vector3Add(position, rotated);
-
+  game -> direction = rotated;
+  RefreshTarget(&game -> camera, rotated);
 }
 
 void up(Game *game) {
-  Vector3 forward = forwardDirection(game);
-  forward = Vector3Normalize(forward);
-  MoveVector(game, forward);
+  MoveVector(game, game -> direction);
 }
 
 void down(Game *game) {
-  Vector3 forward = forwardDirection(game);
-  Vector3 down = Vector3Normalize(forward);
+  Vector3 down = Vector3Normalize(game -> direction);
   down = Vector3Scale(down, -1);
 
   MoveVector(game, down);
 }
 
 void right(Game *game) {
-  Vector3 forward = forwardDirection(game);
-  Vector3 right = Vector3Normalize(forward);
-  right = Vector3Perpendicular(forward);
-
+  Vector3 right = Vector3Perpendicular(game -> direction);
   MoveVector(game, right);
 }
 
 void left(Game *game) {
-  Vector3 forward = forwardDirection(game);
-  Vector3 left = Vector3Normalize(forward);
-  left = Vector3Perpendicular(forward);
+  Vector3 left = Vector3Perpendicular(game->direction);
   left = Vector3Scale(left, -1);
-
   MoveVector(game, left);
 }
 
@@ -133,7 +128,7 @@ void HandleMouse(Game *game) {
     return;
   }
 
-  Flip(game, pos.x);
+  flip(game, pos.x * -1.0f);
 
 }
 
